a7f716b612d8e7d8b9b84848e492c3d2afa15f81
Yoann Lagache <lagache.yoann@gmail.com>
2013-12-05 22:44:03 +0100

    Affichage de 5 produits ok.

--- public/javascripts/frontend/application.js
+++ public/javascripts/frontend/application.js
@@ -1,20 +1,25 @@
+var foo;
 $(function(){
 	 function getFiveProducts() {
+	 	console.log("bar");
 		$.ajax({
-					url: "http://geekstudentinside.herokuapp.com/getProducts", 
+					url: "/getProducts", 
 					type: "GET",
-					dataType:"json",			
-					crossDomain: true,
-					headers: { 'Access-Control-Allow-Origin': '*' },
-					complete: function(data) {
-						for(var article in data) {
-							var divArticle = $("<div/>",{class:"article"});
-							divArticle.append($("<p/>", {class:"article_name"}).append(article.name));
+					dataType:"json"
+					}).done(function(data) {
+						foo = data;						
+						console.log(data);
+						for(var i = 0 ; i < data.length ; ++i) {
+							var article = data[i];
+							/*var divArticle = $("<div/>",{class:"article"});
+							divArticle.append($("<p/>", {class:"article_name"}, {html:article.name}); //.html(article.name));
 							divArticle.append($("<img/>", {class:"article_url"}, {src:article.url}));
-							divArticle.appendTo($(document.body));
-						}
-					}
-			});
+							divArticle.appendTo($(document.body));*/
+							var divArticle = $('<div class="article"></div>');
+							divArticle.append($('<p class="article_name">' + article.name + '</p>'));
+							divArticle.append($('<img src="' + article.url + '" class="article_url"/>'));
+							$('body').append(divArticle);
+						}});
 	 }
 	 getFiveProducts();
 });
