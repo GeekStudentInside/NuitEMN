dff7d7d7802e807c797f2fbd20ee71b777c5d1ef
arni360 <allereims@hotmil.com>
2013-12-05 18:53:58 +0100

    Model
    1er Version

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import models.Article;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        List<Article> articles= Article.nbArticles(5);
+
+        return ok(index.render(articles));
     }
+
+
 
 }
---
+++ app/models/Article.java
@@ -0,0 +1,43 @@
+package models;
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by Harold on 05/12/13.
+ */
+@Entity
+@Table(name = "Article")
+public class Article extends Model {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    Long id;
+    @Column(name="name")
+    public String name;
+    @Column(name="url")
+    public String url;
+
+    public Article(String name,String url){
+        this.url=url;
+        this.name=name;
+    }
+    public static Finder<Long,Article> find = new Finder<Long,Article>(
+            Long.class ,Article.class
+    );
+
+    public static List<Article> nbArticles(int n){
+        List<Article> articles= Article.find.all();
+        int a= articles.size();
+        int c=(int) Math.random()*a;
+        if(c+n-1>a){
+            c=c-n+1;
+        }
+
+        articles.subList(c, c + n-1);
+
+
+
+    }
+}
