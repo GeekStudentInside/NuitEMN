c277cd77e4b2c25549433a121c11937729b79fe4
Guillaume Blondeau <blondeau.gui@gmail.com>
2013-12-05 13:36:10 +0100

    Remove comma in db script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,8 +4,7 @@ CREATE TABLE users (
 
     email VARCHAR (100) PRIMARY KEY,
     name VARCHAR (255),
-    password VARCHAR (255),
-
+    password VARCHAR (255)
 );
 
 INSERT INTO  users (email, name, password) values ('blondeau.gui@gmail.com', 'Guillaume Blondeau', 'test');
