d768102d85de2e5ff7eb70cb7f431d48a6593452
arni360 <allereims@hotmil.com>
2013-12-06 00:53:44 +0100

    Test V1
    Test de l'article

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,10 +4,10 @@ import models.Article;
 import play.*;
 import play.libs.Json;
 import play.mvc.*;
-
 import views.html.*;
 
 import java.util.List;
+
 
 public class Application extends Controller {
 
