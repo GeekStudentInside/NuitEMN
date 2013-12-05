1bc2981ad4a625e7fa3d17d34cec25777d9092c8
Okteril <toxic_c@live.fr>
2013-12-05 20:17:00 +0100

    added style for selected article (clicked)

--- public/javascripts/articleDetails.js
+++ public/javascripts/articleDetails.js
@@ -3,7 +3,7 @@
  */
 function visibleDetails()
 {
-    var article = this//.parentNode;
+    var article = this;
     var hidden = article.getElementsByTagName('p')[0].style.display == 'none';
     var newStyle = hidden ? 'block' : 'none';
 
@@ -15,9 +15,24 @@ function visibleDetails()
     article.getElementsByClassName('article_img')[0 ].style.display = 'block';
 }
 
+function selectedArticle()
+{
+    var article = this;
+    var selected = article.classList.contains('activated');
+    if(selected)
+    {
+        article.classList.add('activated');
+    }
+    else
+    {
+        article.classList.remove('activated');
+    }
+}
+
 var products = $('.article');
 for(var i = 0 ; i < products.length ; ++i)
 {
-    products[i ].addEventListener('mouseover', visibleDetails);
-    products[i ].addEventListener('mouseout', visibleDetails);
+    products[i].addEventListener('mouseover', visibleDetails);
+    products[i].addEventListener('mouseout', visibleDetails);
+    products[i].addEventListener('click', selectedArticle);
 }
