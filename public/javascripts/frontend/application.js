c0c06532ae3e84778644b332816d845fde9b5127
Yoann Lagache <lagache.yoann@gmail.com>
2013-12-06 00:42:22 +0100

    Ajout classe css pour article et dimensionnement des articles pour faire des blocs de 9 dans application.js

--- public/javascripts/frontend/application.js
+++ public/javascripts/frontend/application.js
@@ -6,7 +6,7 @@ $(function(){
             dataType:"json",
             success: function(data) {
                 $.each(data, function(i, article){
-                    var divArticle = $("<div/>", {class:"article"});
+                    var divArticle = $("<div/>", {class:"article large-4"});
                     divArticle.append($("<p/>", {class:"article_name"}).append(article.name));
                     divArticle.append($("<img/>", {class:"article_url"}).attr("src", article.url));
                     divArticle.appendTo($(document.body));
