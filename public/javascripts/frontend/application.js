7f97ebdfae224ff5814b553b7ad06dee598d25ae
Yoann Lagache <lagache.yoann@gmail.com>
2013-12-05 21:25:49 +0100

    premerge backend

--- public/javascripts/frontend/application.js
+++ public/javascripts/frontend/application.js
@@ -1,15 +1,20 @@
 $(function(){
-   function getFiveProducts() {
-        $.getJSON({
-           url: "products.json",
-           context: document.body
-        }).done(function(data) {
-           for(var article in data) {
-               var divArticle = $("<div/>",{class:"article"});
-               divArticle.append($("<p/>", {class:"article_name"}).append(article.name));
-               divArticle.append($("<img/>", {class:"article_url"}, {src:article.url}));
-               divArticle.appendTo($(document.body));
-           }
-        });
-   }
+	 function getFiveProducts() {
+		$.ajax({
+					url: "http://geekstudentinside.herokuapp.com/getProducts", 
+					type: "GET",
+					dataType:"json",			
+					crossDomain: true,
+					headers: { 'Access-Control-Allow-Origin': '*' },
+					complete: function(data) {
+						for(var article in data) {
+							var divArticle = $("<div/>",{class:"article"});
+							divArticle.append($("<p/>", {class:"article_name"}).append(article.name));
+							divArticle.append($("<img/>", {class:"article_url"}, {src:article.url}));
+							divArticle.appendTo($(document.body));
+						}
+					}
+			});
+	 }
+	 getFiveProducts();
 });
