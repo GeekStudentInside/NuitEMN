db9fb254bb6148eccdb519900e771111537b2c0c
arni360 <allereims@hotmil.com>
2013-12-05 19:06:41 +0100

    Back V2
    Back Version1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import models.Article;
+import org.codehaus.jettison.json.JSONObject;
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
@@ -12,10 +14,11 @@ public class Application extends Controller {
 
     public static Result index() {
         List<Article> articles= Article.nbArticles(5);
-
         return ok(index.render(articles));
     }
-
+    public static Result getArticle(){
+        return ok(Json.toJson(Article.nbArticles(5)));
+    }
 
 
 }
