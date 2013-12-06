7b4b7662ee1bdfee6c39aaefc73b545f9777c08b
Okteril <toxic_c@live.fr>
2013-12-06 04:44:35 +0100

    Revert "massive update"
    This reverts commit 08685eb542015d62de64a75ba905358f11a39c6a.

--- public/javascripts/articleDetails.js
+++ public/javascripts/articleDetails.js
@@ -3,32 +3,46 @@
  */
 function visibleDetails()
 {
-    var article = $(this);
-    var hidden = article.find('p').first().css('display') == 'none';
+    var article = this;
+    var hidden = article.getElementsByTagName('p')[0].style.display == 'none';
     var newStyle = hidden ? 'block' : 'none';
 
-    article.children().each(function(index, child){
-        if(!$(child).hasClass('article_url'))
-            $(child).css('display', newStyle);
-    });
+    for(var i = 0 ; i < article.children.length ; ++i)
+    {
+        article.children[i].style.display = newStyle;
+    }
+    article.getElementsByClassName('article_url')[0 ].style.display = 'block';
 }
 
-function selectArticle()
+function selectedArticle()
 {
-    var article = $(this);
-    var selected = article.hasClass('activated')
-    if(selected)
-        article.removeClass('activated')
+    var article = this;
+    var selected = article.classList.contains('activated');
+    if(!selected)
+    {
+        article.classList.add('activated');
+    }
     else
-        article.addClass('activated')
+    {
+        article.classList.remove('activated');
+    }
 }
 
 function getSelectedArticles()
 {
+    var articles = $('.article');
     var selectedArticles = new Array();
-    $('.article.activated').each(function(index, article){
-        selectedArticles.push([$(article).find('.article_name').html(), $(article).find('.article_url').attr('src')]);
-    });
+    for(var i = 0 ; i < articles.length ; ++i)
+    {
+        var article = articles[i];
+        if(article.classList.contains('activated'))
+            {
+            selectedArticles.push(new Array(
+                '' + article.getElementsByClassName('article_name')[0].innerHTML + '',
+                '' + article.getElementsByClassName('article_url')[0].src + ''
+            ));
+        }
+    }
     return selectedArticles;
 }
 
@@ -37,11 +51,11 @@ function sendSelectedArticles()
     var selectedArticles = getSelectedArticles();
     addToHistory();
     /*$.ajax(
-     {url : "selectedArticlesURL",
-     data : selectedArticles}
-     ).done(function (){
-     addToHistory();
-     })*/
+        {url : "selectedArticlesURL",
+    data : selectedArticles}
+    ).done(function (){
+            addToHistory();
+        })*/
 }
 
 function addToHistory()
@@ -50,23 +64,47 @@ function addToHistory()
     var urlArray = new Array();
     var selectedArticles = getSelectedArticles();
 
-    $.each(selectedArticles, function(index, article){
-        urlArray.push(article[1]);
-    });
-
-    $.each(urlArray, function(index, url){
-        var img = $('<a><img src="' + url + '" class="history_img"/></a>');
+    for(var i = 0 ; i < selectedArticles.length ; ++i)
+    {
+        urlArray.push(selectedArticles[i][1]);
+    }
+    for(var i = 0 ; i < urlArray.length ; ++i)
+    {
+        var url = urlArray[i];
+        var img = $('<a><img src="' + url + '" class="history_img" style="height : 100%"/></a>');
         history.append(img);
-    });
+    }
 }
 
-$(function(){
-    getFiveProducts();
-    $(window).on("load", function() {
+document.addEventListener('DOMContentLoaded', function(){
+    /*$.when(onload()).done(function(){
         var articles = $('.article');
-        console.log(articles)
-        articles.each(function(index, valeur){
-            $(this).on('mouseover', visibleDetails).on('mouseout', visibleDetails).on('click', selectArticle);
-        });
+        console.log($('.article'));
+        for(var i = 0 ; i < articles.length ; ++i)
+        {
+            articles[i].addEventListener('mouseover', visibleDetails);
+            articles[i].addEventListener('mouseout', visibleDetails);
+            articles[i].addEventListener('click', selectedArticle);
+        }
+    });*/
+    onload();
+    $(window).bind("load", function() {
+        var articles = $('.article');
+        for(var i = 0 ; i < articles.length ; ++i)
+        {
+            articles[i].addEventListener('mouseover', visibleDetails);
+            articles[i].addEventListener('mouseout', visibleDetails);
+            articles[i].addEventListener('click', selectedArticle);
+        }
     });
+
 });
+
+/*setTimeout(function(){var articles = $('.article');
+    console.log($('.article'));
+    for(var i = 0 ; i < articles.length ; ++i)
+    {
+        articles[i].addEventListener('mouseover', visibleDetails);
+        articles[i].addEventListener('mouseout', visibleDetails);
+        articles[i].addEventListener('click', selectedArticle);
+    }}, 1000);*/
