350fd9222438a1e84ab8f865c04fb656f1770955
Yoann Lagache <lagache.yoann@gmail.com>
2013-12-06 01:09:45 +0100

    cleaning method in application.js

--- public/javascripts/frontend/application.js
+++ public/javascripts/frontend/application.js
@@ -1,6 +1,7 @@
 $(function(){
-
-	function getFiveProducts() {
+//Called in articleDetails.js when DOM is loaded
+function onload(){
+	 function getFiveProducts() {
 		$.ajax({
 			url: "/getProducts", 
 			type: "GET",
@@ -9,7 +10,8 @@ $(function(){
 				$.each(data, function(i, article){
 					var divArticle = $("<div/>", {class:"article large-4"});
 					divArticle.append($("<p/>", {class:"article_name"}).append(article.name));
-					divArticle.append($("<img/>", {class:"article_url"}).attr("src", article.url));
+					divArticle.append($("<img/>", {class:"article_url"}).attr("src", /*article.url*/"http://placekitten.com/200/300"));
+					// var imgArticle = '<img src="' + /*article.url*/'http://placekitten.com/200/300' + '" class="article_url"/>';
 					divArticle.appendTo($(document.body));
 					//au clic sur un article changement de style
 					divArticle.on("click", function() {
@@ -20,6 +22,6 @@ $(function(){
 			}
 		});
 	}
-	getFiveProducts();	
+	getFiveProducts();
 
 });
